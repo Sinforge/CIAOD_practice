a6e0492039c0be8052202f672c57efa4b819eca0
Vlad <vlad.vlasov77@mail.ru>
2022-03-10 23:56:46 +0300

    add third practice code

---
+++ Practice_3/Exercise_1/Exercise_1.cpp
@@ -0,0 +1,56 @@
+﻿#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+#endif
+
+
+
+#include <iostream>
+#include <string.h>
+#include <cstring>
+#include <stdio.h>
+
+using namespace std;
+
+int translate_to_10(char* str);
+
+int find_numbers(char* s1, char* raz, int* mas, int& j);
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	char s[256];
+	int mas[256];
+	char raz[] = " ,";
+	int j = 0;
+	cout << "Введите предложение не больше(256 символов):\n";
+	cin.getline(s, 256, '\n');
+	char* s1 = strtok(s, raz);
+	j = find_numbers(s1, raz, mas, j);
+	cout << "Числа, найденные в тексте и переведнные в 10-ую сс: ";
+	for (int i = 0; i < j; i++) {
+		cout << mas[i] << " ";
+	}
+
+
+}
+
+
+int translate_to_10(char* str) {
+	int number_10 = 0;
+	for (int i = 1; i < strlen(str); i++) {
+		number_10 += (str[i] - '0') * pow(8, strlen(str) - 1 - i);
+	}
+	return number_10;
+}
+
+
+int find_numbers(char* s1, char* raz, int* mas, int& j) {
+	while (s1 != NULL) {
+		if (s1[0] == '$') {
+			mas[j] = translate_to_10(s1);
+			j++;
+		}
+		s1 = strtok(NULL, raz);
+	}
+	return j;
+}
---
+++ Practice_3/Exercise_2/Exercise_2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int translate_to_10(string str);
+
+int find_numbers(string s, string raz, int* mas, int& j);
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	string s, raz = " ,";
+	int j = 0;
+	int mas[256];
+	cout << "Введите предложение:\n";
+	getline(cin, s);
+	j = find_numbers(s, raz, mas, j);
+	cout << "Числа, найденные в тексте и переведнные в 10-ую сс: ";
+	for (int i = 0; i < j; i++) {
+		cout << mas[i] << " ";
+	}
+}
+
+
+int translate_to_10(string str) {
+	int number_10 = 0;
+	for (int i = 1; i < str.size(); i++) {
+		number_10 += (str[i] - '0') * pow(8, str.size() - 1 - i);
+	}
+	return number_10;
+}
+
+int find_numbers(string s, string raz, int* mas, int& j)
+{
+	size_t start_index = 0, end_index;
+	while (start_index != string::npos) {
+		end_index = s.find_first_of(raz, start_index);
+		if (s[start_index] == '$') {
+			mas[j] = translate_to_10(s.substr(start_index, (end_index - start_index)));
+			j++;
+		}
+		start_index = s.find_first_not_of(raz, end_index);
+	}
+	return j;
+}
