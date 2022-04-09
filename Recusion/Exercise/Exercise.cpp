90a83c06207293bac967edef7e2572a497069b07
Sinforge <vlad.vlasov77@mail.ru>
2022-04-09 23:36:29 +0300

    finally end 6th practice

--- Recusion/Exercise/Exercise.cpp
+++ Recusion/Exercise/Exercise.cpp
@@ -3,13 +3,42 @@
 #include <iostream>
 using namespace std;
 
-struct stack {
-    int a;
-    stack* next;
+struct stack { // стек( информационная часть узла - a, добавил просто для наглядности; 
+    int a; // информационная часть узла, добавил просто для наглядности; 
+    stack* next = NULL;// указатель на следующий элемент - next
 };
 
 
-float average(float * mas, int start, int end) {
+float average(float* mas, int start, int end);//Рекурсивная функция подсчета среднего арифм.
+
+void create_stack(stack* last_elem, int size);// Создание стек, состоящего из n узлов
+void check_stack(stack* STACK); //Вывод стека на экран
+//Вариант-5
+int main()
+{
+    setlocale(LC_ALL, "");
+    //задание 1
+    float* mas;
+    int size;
+    cout << "введите размер массива: ";
+    cin >> size;
+    mas = new float[size];
+    for (int i = 0; i < size; i++) {
+        cin >> mas[i];
+    }
+    cout << "average:" <<  average(mas, 0, size) << endl;
+    //задание 2
+    int size_stack;
+    stack* NewStack = new stack();
+    cout << "\nВведите кол-во элементов в стеке: ";
+    cin >> size_stack;
+    create_stack(NewStack, size_stack);
+    check_stack(NewStack);
+
+}
+
+
+float average(float* mas, int start, int end) {//Рекурсивная функция подсчета среднего арифм.
     static unsigned int count = 0;
     if (end != start) {
         count++;
@@ -21,37 +50,24 @@ float average(float * mas, int start, int end) {
     }
 }
 
-void create_stack(stack* last_elem, int size) {
+void create_stack(stack* last_elem, int size) {// Создание стек, состоящего из n узлов
     static unsigned int count = 0;
     if (size > 0) {
         count++;
         stack* node = new stack();
         last_elem->next = node;
+        last_elem->a = size;
         create_stack(node, size - 1);
     }
     else {
-        cout << count << endl;
+        cout <<"Глубина рекурсии " <<  count << endl;
     }
 
 }
-//Вариант-5
-int main()
-{
-    setlocale(LC_ALL, "");
-    //Задание 1
-    float* mas;
-    int size;
-    cout << "Введите размер массива: ";
-    cin >> size;
-    mas = new float[size];
-    for (int i = 0; i < size; i++) {
-        cin >> mas[i];
+void check_stack(stack* STACK) {
+    stack* node = STACK->next;
+    while (node != NULL) {
+        cout << node->a << " ";
+        node = node->next;
     }
-    cout << "Average:" <<  average(mas, 0, size) << endl;
-    //Задание 2
-    int size_stack;
-    stack* NewStack = new stack();
-    cin >> size_stack;
-    create_stack(NewStack, size_stack);
-
 }
