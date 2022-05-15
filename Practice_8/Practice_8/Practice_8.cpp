aa2c420cf4581b503ff9149fc208c8d5cb86ab72
Sinforge <vlad.vlasov77@mail.ru>
2022-05-15 14:32:14 +0300

    change natural merge sort

--- Practice_8/Practice_8/Practice_8.cpp
+++ Practice_8/Practice_8/Practice_8.cpp
@@ -1,11 +1,10 @@
-﻿// Practice_8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
 #include <string>
-#include <fstream>
 #include <cstdio>
+#include <time.h>
 using namespace std;
 
 
@@ -21,14 +20,14 @@ void fill_handwrite_vector(vector<int>& mas) {
     }
 }
 void print_vector(vector<int> mas) {
-    cout << "\nМассив(размер = " << mas.size() << 
+    cout << "\nМассив(размер = " << mas.size() <<
         "): ";
     for (int i = 0; i < mas.size(); i++) {
         cout << mas[i] << " ";
     }
 }
 int insert_sort(vector<int>& mas) {
-    unsigned int start_time = clock();
+    double start_time = clock();
     for (int i = 1; i < mas.size(); i++) {
         int x = mas[i];
         int j = i;
@@ -38,13 +37,13 @@ int insert_sort(vector<int>& mas) {
         }
         mas[j] = x;
     }
-    unsigned int end_time = clock();
+    double end_time = clock();
     return end_time - start_time;
 }
-int Shell_sort(vector<int> & mas) {
+int Shell_sort(vector<int>& mas) {
     unsigned int start_time = clock();
     int h = 1;
-    while (h < mas.size()/2) {
+    while (h < mas.size() / 2) {
         h = 2 * h + 1;
     }
     while (h > 0) {
@@ -55,76 +54,88 @@ int Shell_sort(vector<int> & mas) {
                 }
                 else {
                     break;
-                 }
                 }
             }
-        h = (h - 1) / 2;
         }
+        h = (h - 1) / 2;
+    }
     unsigned int end_time = clock();
     return (end_time - start_time);
 }
 
 
-vector<int> merge(vector<int> v1, vector<int> v2) {
-    vector<int> result;
-    int it1 =0 , it2 =0 ;
-    while (true) {
-        if (it1 == v1.size()) {
-            for (int i = it2; i < v2.size(); i++) {
-                result.push_back(v2[i]);
-            }
-            break;
-        }
-        if (it2 == v2.size()) {
-            for (int i = it1; i < v1.size(); i++) {
-                result.push_back(v1[i]);
-            }
-            break;
-        }
-        if (v1[it1] <= v2[it2]) {
-            result.push_back(v1[it1]);
-            it1++;
+void merge(vector<int>& source, vector<int>& target, int startleft, int startright, int endright) {
+    int leftpos = startleft;
+    int rightpos = startright;
+    int targetpos = startleft;
+
+
+    while (leftpos < startright && rightpos < endright) {
+        int leftValue = source[leftpos];
+        int rightValue = source[rightpos];
+        if (leftValue <= rightValue) {
+            target[targetpos++] = leftValue;
+            leftpos++;
         }
         else {
-            result.push_back(v2[it2]);
-            it2++;
+            target[targetpos++] = rightValue;
+            rightpos++;
         }
     }
-    return result;
+
+    while (leftpos < startright) {
+        target[targetpos++] = source[leftpos++];
+    }
+    while (rightpos < endright) {
+        target[targetpos++] = source[rightpos++];
+    }
 }
 
 
-//Естественное слияние
-int Natural_Merge_sort(vector<int> &mas) {
-    unsigned int start = clock();
-    vector<vector<int>> sub_mas(1);
-    int a = 0; //номер подпоследовательности
-    sub_mas[0].push_back(mas[0]);
+void NaturalMergeSort(vector<int>& mas) {
+    vector<int> segments = vector<int>(mas.size() + 1);
 
-    //Разделяем на подпоследовательности
-    for (int i = 1; i < mas.size(); i++) {
-        if (mas[i] >= mas[i - 1]) {
-            sub_mas[a].push_back(mas[i]);
-        }
-        else {
-            vector<int> subseq;
-            sub_mas.push_back(subseq);
-            a++;
-            sub_mas[a].push_back(mas[i]);
+    int segCount = 0;
+    segments[0] = 0;
+
+    //Определение подпоследовательностей
+    for (int i = 1; i <= mas.size(); i++) {
+        if (i == mas.size() || mas[i] < mas[i - 1]) {
+            segments[++segCount] = i;
         }
     }
-    while (sub_mas.size() != 1) {
-        vector<vector<int>> s;
-        for (int i = 0; i < sub_mas.size() - 1; i += 2) {
-            s.push_back(merge(sub_mas[i], sub_mas[i + 1]));
-        }
-        sub_mas = s;
-        
 
+    vector<int> from = mas;
+    vector<int> to = vector<int>(mas.size());
+    while (segCount > 1) {
+        int new_segCount = 0;
+        for (int i = 0; i < segCount - 1; i += 2) {
+            merge(from, to, segments[i], segments[i + 1], segments[i + 2]);
+            segments[new_segCount++] = segments[i];
+        }
+
+
+        if (segCount % 2 == 1) {
+            int lastSeg = segments[segCount - 1];
+            for (int i = lastSeg; i < mas.size(); i++) {
+                to[i] = from[i];
+            }
+            segments[new_segCount++] = lastSeg;
+        }
+
+        segments[new_segCount] = mas.size();
+        segCount = new_segCount;
+
+        vector<int> help = from;
+        from = to;
+        to = help;
     }
-    mas = sub_mas[0];
-    unsigned int end = clock();
-    return end - start;
+    if (from != mas) {
+        for (int i = 0; i < mas.size(); i++) {
+            mas[i] = from[i];
+        }
+    }
+
 }
 
 
@@ -163,21 +174,18 @@ int main()
             cout << "Время выполнения :" << Shell_sort(mas) << " мс\n";
             break;
         case 7:
-            cout << "Время выполнения :" << Natural_Merge_sort(mas) << " мс\n";
+            start = clock();
+            NaturalMergeSort(mas);
+            end = clock();
+            cout << "Время выполнения :" << end -start << " мс\n";
+            break;
+        case 8:
+            reverse(mas.begin(), mas.end());
             break;
         }
+        
 
         cout << "\n\nПользовательское меню:\n0- Выход из программы\n1- Установить размер массива;\n2- Заполнить массив вручную;\n3 - Заполнить массив псевдорандомными числами;\n4-Вывести массив;\n5-Сортировка вставками;\n6-Сортировка Шелла со смещения Кнута(2 способ)\n7-Естественное слияние;\n";
         cin >> menu;
     }
 }
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
